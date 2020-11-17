e9930efdde31b4a31c9d1345f663d585f32dfc96
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-17 09:55:17 -0700

    simple script to send an email to my own gmail account

---
+++ application/views/pages/email.php
@@ -0,0 +1,15 @@
+<h2><?= $title ?></h2>
+<p> Check Email </p>
+
+<?php 
+$to       = 'aazambrano201@gmail.com';
+$subject  = 'Testing sendmail.exe';
+$message  = 'Hi, you just received an email using sendmail!';
+$headers  = 'From: aazambrano201@@gmail.com' . "\r\n" .
+            'MIME-Version: 1.0' . "\r\n" .
+            'Content-type: text/html; charset=utf-8';
+if(mail($to, $subject, $message, $headers))
+    echo "Email sent";
+else
+    echo "Email sending failed";
+?>
