fb63a71d9d885dedaa3716320109cd90dadf4918
aazambrano2 <aazambrano2@miners.utep.edu>
2020-11-18 17:02:52 -0700

    Attempting to simulate getting a unique url to verify an email address.

--- application/views/pages/email.php
+++ application/views/pages/email.php
@@ -2,9 +2,10 @@
 <p> Check Email </p>
 
 <?php 
+$url = com_create_guid();
 $to       = 'aazambrano201@gmail.com';
 $subject  = 'Testing sendmail.exe';
-$message  = 'Hi, you just received an email using sendmail!';
+$message  = 'Hi, you just received an email using sendmail! Click this link to verify your account <a href= http://localhost/aaron-personal-codeigniter/verified >'.$url.'<a/>';
 $headers  = 'From: aazambrano201@@gmail.com' . "\r\n" .
             'MIME-Version: 1.0' . "\r\n" .
             'Content-type: text/html; charset=utf-8';
