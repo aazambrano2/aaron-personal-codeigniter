87406ef24a3e6946b9fbb677429b14734ebadba2
aazambrano2 <aazambrano2@miners.utep.edu>
2021-09-03 14:38:13 -0600

    minor change

--- application/views/pages/email.php
+++ application/views/pages/email.php
@@ -5,8 +5,8 @@
 $url = com_create_guid();
 $to       = 'aazambrano201@gmail.com';
 $subject  = 'Testing sendmail.exe';
-$message  = 'Hi, you just received an email using sendmail! Click this link to verify your account <a href= http://localhost/aaron-personal-codeigniter/verified >'.$url.'<a/>';
-$headers  = 'From: aazambrano201@@gmail.com' . "\r\n" .
+$message  = 'Hi, you just received an email using sendmail! Click this link to verify your account <a href= http://localhost/aaron-personal-codeigniter/verified >hello<a/>';
+$headers  = 'From: aazambrano201@gmail.com' . "\r\n" .
             'MIME-Version: 1.0' . "\r\n" .
             'Content-type: text/html; charset=utf-8';
 if(mail($to, $subject, $message, $headers))
